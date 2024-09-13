ab04601fcd0dd0d4fa38260bb0fe1576a6c96503
SamTheMan17 <sblondeau0001@mymail.lausd.net>
2024-09-13 12:56:36 -0700

    main

--- PainterPlus.java
+++ PainterPlus.java
@@ -1,99 +1,45 @@
 import org.code.neighborhood.*;
 
-public class MinecraftPainter extends MuralPainter {
+public class PainterPlus extends Painter {
 
-  /*Method to draw the entire villager character
-  this helps to create the full face of the villager using the second painter*/
-  public void drawVillager() {
-   drawskin(); // Draws the skin section of villager
-   draweyesandrestofskin(); // Draws the eyes and remaining skin details
+  // Turns the Painter to the right
+  public void turnRight() {
+    turnLeft();
+    turnLeft();
+    turnLeft();
   }
-  // Method to paint the skin of villager
 
- public void drawskin(){
-turnRight();
-paintLine("LightSalmon", 3); // Paint the top part of the skin
-turnLeft();
-move();
-move();
-turnLeft();
-move();
-move();
-move();
-paintLine("LightSalmon", 1); // Paint the left side of the face
-turnRight();
-move();
-move();
-move();
-move();
-turnRight();
-paintLine("LightSalmon", 3); // Paint the bottom part of the face
-turnRight();
-paintLine("black", 6);   // Outline the eyebrows
-turnLeft();
-move();
-paintLine("LightSalmon", 1); 
-paintLine("sienna", 3); 
-turnLeft();
-move();
-turnLeft();
-paintLine("LightSalmon", 3);
+  // Choice A: Takes all paint from a paint bucket
+  public void takeAllPaint() {
+    while (isOnBucket()) {
+      takePaint();
+    }
+  }
 
- }
-   //Paints rest of facial features
- public void draweyesandrestofskin() {
-   
-paintLine("white", 1); //Paint EYES
-turnRight();
-move();
-turnRight();
-move();
-turnLeft();
-paintLine("ForestGreen", 1); //Paint EYES
-move();
-move(); 
-paintLine("ForestGreen", 1); //Paint EYES
-paintLine("white", 1); //Paint EYES
-paintLine("LightSalmon", 1); //Rest is to finish face details
-turnRight();
-move();
-paintLine("sienna", 3);
-turnRight();
-move();
-turnRight();
-paintLine("LightSalmon", 3);
-turnLeft();
-turnLeft();
-move();
-turnRight();
-move();
-move();
-turnLeft();
-paintLine("sienna", 3);
-turnRight();
-move();
-turnRight();
-paintLine("sienna", 3);
-turnLeft();
-move();
-turnLeft();
-move();
-move();
-paintLine("brown", 1); //Paint for nose
-paintLine("LightSalmon", 1);
-turnLeft();
-move();
-move();
-move();
-turnLeft();   
-paintLine("LightSalmon", 1);
-paintLine("brown", 1); //Paint for nose
-turnRight(); //Sets painter back with other painter
-move();
-move();
-turnRight();
-move();
-move();
+  // Choice B: Moves forward while a Painter object can move
+  public void moveFast() {
+    while (canMove()) {
+      move();
+    }
+  }
 
- } 
-   }  
+  // Choice C: Paints and moves while the Painter object has paint
+  public void paintToEmpty(String color) {
+    while (hasPaint()) {
+      paint(color);
+      move();
+    }
+  }
+
+  // Choice D: Moves, turns, and paints in a donut shape
+  public void paintDonut(String color) {
+    while (hasPaint()) {
+      move();
+      turnRight();
+      paint(color);
+      move();
+      paint(color);
+    }
+  }
+  
+}
